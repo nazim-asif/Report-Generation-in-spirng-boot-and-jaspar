5438b1b003088ef9b538941bc8ad0c573a5ae345
nazim-asif <imnazimuddinasif@gamil.com>
2021-03-31 22:08:13 +0600

    documentation and thanks letter template done

--- src/main/java/com/jasperreportusingjava/jasperrepot/service/EmployeeService.java
+++ src/main/java/com/jasperreportusingjava/jasperrepot/service/EmployeeService.java
@@ -6,6 +6,7 @@ import com.jasperreportusingjava.jasperrepot.repository.EmployeeRepository;
 import net.sf.jasperreports.engine.*;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 import net.sf.jasperreports.engine.export.JRCsvExporter;
+import net.sf.jasperreports.engine.export.JRXlsExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
 import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
 import net.sf.jasperreports.export.*;
@@ -29,20 +30,38 @@ public class EmployeeService {
     @Autowired
     private EmployeeRepository repository;
 
+    /**
+     * This method is customizing the data for jrxml file.
+     * @param map it takes the value of jrxmlFilePath,
+     *            reportFormat,reportName and destinationPath from user
+     * @return It returns destination path of downloaded file
+     * @throws FileNotFoundException
+     * @throws JRException
+     */
+
     public String getCustomisedDataForReport(Map map) throws FileNotFoundException, JRException {
         Map<String, Object> parameters = new HashMap<>();
-        parameters.put("name", "Nazim Uddin Asif");
-        parameters.put("reportTitle", "Employee Info");
-        parameters.put("designation", "Manager");
-        parameters.put("reportDescription", "Employee Information Report");
-
-        List<Employee> employees = repository.findAll();
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
-        parameters.put("tblData", dataSource);
+//        parameters.put("name", "Nazim Uddin Asif");
+//        parameters.put("reportTitle", "Employee Info");
+//        parameters.put("designation", "Manager");
+//        parameters.put("reportDescription", "Employee Information Report");
+//
+//        List<Employee> employees = repository.findAll();
+//        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+//        parameters.put("tblData", dataSource);
 
         return generateReport(map, parameters);
     }
 
+    /**
+     * Fill the report format and call a method for generating file
+     * @param map it takes values of jrxmlFilePath,
+     *            reportFormat,reportName and destinationPath from user
+     * @param parameters it takes values for fill up jrxml template
+     * @return destination path of downloaded file
+     * @throws FileNotFoundException
+     * @throws JRException
+     */
     public String generateReport(Map map, Map parameters) throws FileNotFoundException, JRException {
         File file = ResourceUtils.getFile("classpath:"+map.get("jrxmlFilePath"));
         JasperReport Report = JasperCompileManager.compileReport(file.getAbsolutePath());
@@ -54,6 +73,16 @@ public class EmployeeService {
         return "report generated in path : " + map.get("destinationPath");
     }
 
+    /**
+     * Generate file
+     * @param reportFormat Name of format
+     * @param jasperPrint fill up template
+     * @param path destination path
+     * @param reportName report name
+     * @throws JRException
+     * @throws FileNotFoundException
+     */
+
     public void getFormattedFile(String reportFormat, JasperPrint jasperPrint, String path, String reportName) throws JRException, FileNotFoundException {
         if (reportFormat.equalsIgnoreCase("html")) {
             JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\"+reportName+".html");
@@ -64,12 +93,32 @@ public class EmployeeService {
 
         }
 
-        else if (reportFormat.equalsIgnoreCase("excel")) {
+        else if (reportFormat.equalsIgnoreCase("xlsx")) {
             JRXlsxExporter exporter = new JRXlsxExporter();
             exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
             exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(
                     new FileOutputStream(
                             path + "\\"+reportName+".xlsx"
+                    )
+            ));
+            exporter.exportReport();
+        }
+        else if (reportFormat.equalsIgnoreCase("xls")) {
+            SimpleXlsReportConfiguration configuration = new SimpleXlsReportConfiguration();
+            configuration.setOnePagePerSheet(true);
+            configuration.setDetectCellType(true);
+            configuration.setWhitePageBackground(false);
+            configuration.setFontSizeFixEnabled(false);
+            configuration.setRemoveEmptySpaceBetweenRows(true);
+            configuration.setRemoveEmptySpaceBetweenColumns(true);
+
+
+            JRXlsExporter exporter = new JRXlsExporter();
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setConfiguration(configuration);
+            exporter.setExporterOutput( new SimpleOutputStreamExporterOutput(
+                    new FileOutputStream(
+                            path + "\\"+reportName+".xls"
                     )
             ));
             exporter.exportReport();
